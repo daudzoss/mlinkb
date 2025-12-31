3fb77e0d26dc107585d117acb0125a1c78390508
Daud Zoss <daud.zoss@gmail.com>
2025-12-30 20:47:39 -0800

    checkok() partially rolled back up to save bytes and use the natural loop over y

--- checksub.asm
+++ checksub.asm
@@ -1,39 +1,46 @@
 tempchk	.fill	1		;
 unsolvd	lda	#0		;
 	rts			;
-checkt	.macro	offset		;inline uint1_t checkt(uint4_t offset) {
-	bit	state+\offset	; if (state[offset] & 0xc0 != 0x80)
+checkt	.macro	offst		;inline uint1_t checkt(uint4_t offst) {
+	bit	state+\offst	; if (state[offst] & 0xc0 != 0x80)
 	bpl	unsolvd		;  return 0; // give up since not a TOPLINK
 	bvs	unsolvd		; else return 1; // otherwise, keep checking
 	.endm			;} // checkt()
-checkb	.macro	offset		;inline uint1_t checkt(uint4_t offset) {
-	bit	state+\offset	; if (state[offset] & 0xc0 != 0xc0)
+checkb	.macro	offst		;inline uint1_t checkt(uint4_t offst) {
+	bit	state+\offst	; if (state[offst] & 0xc0 != 0xc0)
 	bpl	unsolvd		;  return 0; // give up since not a BOTLINK
 	bvc	unsolvd		; else return 1; // otherwise, keep checking
 	.endm			;} // checkb()
-checkc	.macro	offset		;inline uint1_t checkc(uint4_t offset) {
-	lda	state+\offset+0	;
+
+checkc	.macro
+	ldy	#0		;uint1_t checkc(void) {
+-	lda	state,y		; for (register uint8_t y = 0; y < 16; y += 4) {
 	and	#$3f		;
-	sta	tempchk		; tempchk = state[offset] & 0x3f; // color bits
-	lda	state+\offset+1	;
-	beq	+		; if (state[offset+1] == 0 ||
-	and	#$3f		;
-	cmp	tempchk		;
-	beq	+		;     (state[offset+1] & 0x3f != tempchk))
-	jmp	unsolvd		;  return 0;
-+	lda	state+\offset+2	;
-	beq	+		; else if (state[offset+2] == 0 ||
+n	sta	tempchk		;  tempchk = state[y] & 0x3f; // color bits
+	iny			;
+	lda	state,y		;
+	beq	+		;  if (state[y+1] == 0 ||
 	and	#$3f		;
 	cmp	tempchk		;
-	beq	+		;     (state[offset+2] & 0x3f != tempchk))
+	beq	+		;      (state[y+1] & 0x3f != tempchk))
+	jmp	unsolvd		;   return 0;
++	iny			;
+	lda	state,y		;
+	beq	+		;  else if (state[y+2] == 0 ||
+	and	#$3f		;
+	cmp	tempchk		;
+	beq	+		;      (state[y+2] & 0x3f != tempchk))
 	jmp	unsolvd		;  return 0;
-+	lda	state+\offset+3	;
-	and	#$3f		; else if (state[offset+3] != tempchk)
-	cmp	tempchk		;  return 0;
-	beq	+		; else
-	jmp	unsolvd		;  return 1;
-+
-	.endm			;} // checkc()
++	iny			;
+	lda	state,y		;
+	and	#$3f		;  else if (state[y+3] != tempchk)
+	cmp	tempchk		;   return 0;
+	beq	+		;  else
+	jmp	unsolvd		;   return 1;
++	iny			;
+	cpy	#$10		; }
+	bcc	-		;} // checkc()
+	.endm
 
 checkok	checkt	0		;uint1_t checkok(void) {
 	checkt	4		; return checkt(0) &&
@@ -43,9 +50,6 @@ checkok	checkt	0		;uint1_t checkok(void) {
 	checkb	7		;        checkb(3) &&
 	checkb	$b		;        checkb(7) &&
 	checkb	$f		;        checkb(11) &&
-	checkc	0		;        checkb(15) && // BOTLINK under columns
-	checkc	4		;        checkc(0) &&
-	checkc	8		;        checkc(4) &&
-	checkc	$c		;        checkc(8) &&
-	lda	#1		;        checkc(12); // one color per column
+	checkc			;        checkb(15) && // BOTLINK under columns
+	lda	#1		;        checkc(); // one color per column
 	rts			;} // checkok()
